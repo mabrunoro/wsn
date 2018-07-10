c8ca63bc66b60d20e89c3730ebbd4269377c89c5
Marco Brunoro <ma.brunoro@gmail.com>
2018-07-10 17:22:02 -0300

    commiting

--- trab/ploting.py
+++ trab/ploting.py
@@ -1,37 +1,65 @@
-import matplotlib
-matplotlib.use('TkAgg')
-import numpy as np
+# import matplotlib
+# matplotlib.use('TkAgg')
+# import numpy as np
+# import matplotlib.pyplot as plt
+# from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
+# from matplotlib.figure import Figure
+# import tkinter as tk
+# import tkinter.ttk as ttk
+# import sys
+#
+# class Application(tk.Frame):
+#     def __init__(self, master=None):
+#         tk.Frame.__init__(self,master)
+#         self.createWidgets()
+#
+#     def createWidgets(self):
+#         fig=plt.figure(figsize=(8,8))
+#         ax=fig.add_axes([0.1,0.1,0.8,0.8],polar=True)
+#         canvas=FigureCanvasTkAgg(fig,master=root)
+#         canvas.get_tk_widget().grid(row=0,column=1)
+#         canvas.show()
+#
+#         self.plotbutton=tk.Button(master=root, text="plot", command=lambda: self.plot(canvas,ax))
+#         self.plotbutton.grid(row=0,column=0)
+#
+#     def plot(self,canvas,ax):
+#         for line in sys.stdout: #infinite loop, reads data of a subprocess
+#             theta=line[1]
+#             r=line[2]
+#             ax.plot(theta,r,linestyle="None",maker='o')
+#             canvas.draw()
+#             ax.clear()
+#             #here set axes
+#
+# root=tk.Tk()
+# app=Application(master=root)
+# app.mainloop()
+
+import pyformulas as pf
 import matplotlib.pyplot as plt
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
-from matplotlib.figure import Figure
-import tkinter as tk
-import tkinter.ttk as ttk
-import sys
+import numpy as np
+import time
 
-class Application(tk.Frame):
-    def __init__(self, master=None):
-        tk.Frame.__init__(self,master)
-        self.createWidgets()
+fig = plt.figure()
 
-    def createWidgets(self):
-        fig=plt.figure(figsize=(8,8))
-        ax=fig.add_axes([0.1,0.1,0.8,0.8],polar=True)
-        canvas=FigureCanvasTkAgg(fig,master=root)
-        canvas.get_tk_widget().grid(row=0,column=1)
-        canvas.show()
+canvas = np.zeros((480,640))
+screen = pf.screen(canvas, 'Sinusoid')
 
-        self.plotbutton=tk.Button(master=root, text="plot", command=lambda: self.plot(canvas,ax))
-        self.plotbutton.grid(row=0,column=0)
+start = time.time()
+while True:
+    now = time.time() - start
 
-    def plot(self,canvas,ax):
-        for line in sys.stdout: #infinite loop, reads data of a subprocess
-            theta=line[1]
-            r=line[2]
-            ax.plot(theta,r,linestyle="None",maker='o')
-            canvas.draw()
-            ax.clear()
-            #here set axes
+    x = np.linspace(now-2, now, 100)
+    y = np.sin(2*np.pi*x) + np.sin(3*np.pi*x)
+    plt.xlim(now-2,now+1)
+    plt.ylim(-3,3)
+    plt.plot(x, y, c='black')
 
-root=tk.Tk()
-app=Application(master=root)
-app.mainloop()
+    # If we haven't already shown or saved the plot, then we need to draw the figure first...
+    fig.canvas.draw()
+
+    image = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8, sep='')
+    image = image.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+
+    screen.update(image)
--- trab/trab.py
+++ trab/trab.py
@@ -2,9 +2,16 @@
 import socket
 import struct
 import math
+import matplotlib.pyplot as plt
+import numpy as np
+# import pyformulas as pf
 
 pkt = ['header', 'daddr', 'lsaddr', 'mlen', 'gid', 'hid',
  		'nid', 'temp', 'hum', 'lum']
+
+def update(hl, data):
+	hl.set_ydata(data)
+	plt.draw()
 
 def humidity(temp,hv):
 	return (-2.0468+0.0367*float(hv) - 0.0000015955*math.pow(float(hv),2.0)) + (temp-25)*(0.01+0.00008*float(hv))
@@ -18,10 +25,19 @@ def convert(data):
 
 def main(ip='192.168.100.112',pn=44000):
 	samples = {}
+
+	fig = plt.figure()
+	# canvas = np.zeros((480,640))
+	# screen = pf.screen(canvas, 'Figura')
+
 	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sck:
 		sck.connect((ip, pn))
 		sck.recv(128)
 		sck.send(b'U ')
+		plt.ion()
+		fig = plt.figure()
+		a1 = fig.add_subplot(1,3,1)
+		a2 = fig.add_subplot
 		while(True):
 			data = sck.recv(128)
 			if(len(data) != 1):
@@ -36,10 +52,30 @@ def main(ip='192.168.100.112',pn=44000):
 					samples[c[6][1]]["lum"].append(c[9][1])
 				else:
 					samples[c[6][1]] = {}
-					samples[c[6][1]]["tem"] = [c[7][1]]
-					samples[c[6][1]]["hum"] = [c[8][1]]
-					samples[c[6][1]]["lum"] = [c[9][1]]
-				print(samples)
+					samples[c[6][1]]["tem"] = ([1] * 100) + [c[7][1]]
+					samples[c[6][1]]["hum"] = ([1] * 100) + [c[8][1]]
+					samples[c[6][1]]["lum"] = ([1] * 100) + [c[9][1]]
+
+				length = len(samples.keys())
+				counter = length*3
+				for id in samples.keys():
+					for i in range(1, counter + 1, 3):
+						plt.subplot(3, length, i)
+						plt.plot(samples[id]["tem"][-100:])
+
+						plt.subplot(3, length, i + 1)
+						plt.plot(samples[id]["hum"][-100:])
+
+						plt.subplot(3, length, i + 2)
+						plt.plot(samples[id]["lum"][-100:])
+
+				plt.show()
+
+				# fig.canvas.draw()
+				# image = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8, sep='')
+				# image = image.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+                #
+				# screen.update(image)
 
 if __name__ == '__main__':
 	main()
