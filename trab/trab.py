8bd6f3e3165430f97401c71d4e347a129382e93e
Marco Brunoro <ma.brunoro@gmail.com>
2018-07-10 15:31:39 -0300

    created dict

--- trab/trab.py
+++ trab/trab.py
@@ -12,11 +12,12 @@ def humidity(temp,hv):
 def convert(data):
 	temp = (float(data[7])*0.01) - 39.4
 	hum = humidity(temp,data[8])
-	for i in pkt:
-		d[i] =
-	#  (float(data[8])*0.0367)-2.0468-0.0000015955*math.pow(float(data[8]),2.0)+
+	# for i in len(pkt):
+		# d[pkt[i]] =
+	return list(zip(pkt,data[:7]+(temp,) + (hum,) + data[9:]))
 
 def main(ip='192.168.100.112',pn=44000):
+	samples = {}
 	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sck:
 		sck.connect((ip, pn))
 		sck.recv(128)
@@ -27,7 +28,18 @@ def main(ip='192.168.100.112',pn=44000):
 				# print(data, len(data))
 				# print(data.hex())
 				recv_data = struct.unpack(">BHHBBBHHHH", data)
-				print(convert(recv_data),'\n')
+				c = convert(recv_data)
+				# [('header', 0), ('daddr', 65535), ('lsaddr', 1), ('mlen', 8), ('gid', 34), ('hid', 1), ('nid', 1), ('temp', 27.729), ('hum', 48.286), ('lum', 14)]
+				if(c[6][1] in samples):
+					samples[c[6][1]]["tem"].append(c[7][1])
+					samples[c[6][1]]["hum"].append(c[8][1])
+					samples[c[6][1]]["lum"].append(c[9][1])
+				else:
+					samples[c[6][1]] = {}
+					samples[c[6][1]]["tem"] = [c[7][1]]
+					samples[c[6][1]]["hum"] = [c[8][1]]
+					samples[c[6][1]]["lum"] = [c[9][1]]
+				print(samples)
 
 if __name__ == '__main__':
 	main()
